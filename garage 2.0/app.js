04edbc8019f5e12434e868e3f30722d642ea431c
Marwan Hilmi <mhilmi@gmail.com>
2012-07-23 00:02:08 -0700

    comments on app.js - basically a list of todos

--- garage 2.0/app.js
+++ garage 2.0/app.js
@@ -1,4 +1,6 @@
-
+//this app.js will run on the 711 Scott network locally
+//it will connected to the arduino via a tcp socket
+//it will connect to the external world via a socket.io, port opened on network
 
 var http = require('http')
   , net = require('net')
@@ -8,10 +10,14 @@ var http = require('http')
   , sys = require(process.binding('natives').util ? 'util' : 'sys')
   , server;
     
-
-tcpGuests = [];
+//List of TCP sockets + socketio web sockets
+//Later shouldn't be nessecary, socketio keeps list of clients
+//Currently crashes because of write to disconnected web socket (they do not get removed from the list on disconnect)
+tcpGuests = []; //also not needed because we should only have the 1 arduino connected
 webGuests = [];
 
+//create http server to send arduino.html 
+//can remove after testing, arduino.html can be loaded locally / anywhere
 server = http.createServer(function(req, res){
   var path = url.parse(req.url).pathname;
   switch (path){
@@ -36,35 +42,41 @@ send404 = function(res){
 
 server.listen(8080);
 
+//create socketio on port 8080
 var io = io.listen(server);
 io.on('connection', function(webSocket){
-
+ 
+  //socketio should have own list of open connections
   webGuests.push(webSocket);
+  //run like basic web sockets
   webSocket.on('message', function(message){
 	console.log(message);
-
     for (g in tcpGuests) {
+		//write to each tcp socket (in our case we should only have the 1 arduino)
         tcpGuests[g].write(message);
     }
   });
 
 });
 
-
+//create tcp socket server on 1337
 var tcpServer = net.createServer(function (tcpSocket) {});
 
 tcpServer.on('connection',function(tcpSocket){
     console.log('num of connections on port 1337: ' + tcpServer.connections);
-    tcpGuests.push(tcpSocket);
+    //also, not needed, we will only have tcp client
+	tcpGuests.push(tcpSocket);
     
+
     tcpSocket.on('data',function(data){
         console.log('received on tcp socket:'+data);
-        
         for (g in webGuests) {
+			//write data directly to the web sockets
             var webSocket = webGuests[g];
             webSocket.send(data.toString('ascii',0,data.length));
             
         }
     })
 });
+
 tcpServer.listen(1337);
