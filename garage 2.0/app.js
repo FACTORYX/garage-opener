b6ad74f98ef57793475679db4a4de5d504ab8586
Turner Bohlen <turnerbohlen@gmail.com>
2012-07-24 15:23:20 -0700

    Changes app.js from http server to middleman
    Missing address for the memento site server. This needs to be filled in.

--- garage 2.0/app.js
+++ garage 2.0/app.js
@@ -1,55 +1,83 @@
-
-
 var http = require('http')
   , net = require('net')
   , url = require('url')
   , fs = require('fs')
-  , io = require('socket.io')
   , sys = require(process.binding('natives').util ? 'util' : 'sys')
   , server;
-    
 
-tcpGuests = [];
-webGuests = [];
+var tcpGuests = [];
+var mementoTCP = null;
+var MEMENTO_SITE_PORT;
+var MEMENTO_SITE_HOST;
 
-server = http.createServer(function(req, res){
-  var path = url.parse(req.url).pathname;
-  switch (path){
-    case '/':
-      fs.readFile(__dirname + '/arduino.html', function(err, data){
-        if (err) return send404(res);
-        res.writeHead(200, {'Content-Type': 'text/html'})
-        res.write(data, 'utf8');
-        res.end();
-      });
-      break;
-      
-    default: send404(res);
-  }
-}),
+if (process.env.NODE_ENV === 'production') {
+    MEMENTO_SITE_PORT = 4116;
+    MEMENTO_SITE_HOST = '';
+}
+else if (process.env.NODE_ENV === 'development') {
+    MEMENTO_SITE_PORT = 4116;
+    MEMENTO_SITE_HOST = '';
+}
+else  {
+    MEMENTO_SITE_PORT = 4116;
+    MEMENTO_SITE_HOST = '';
+}
 
-send404 = function(res){
-  res.writeHead(404);
-  res.write('404');
-  res.end();
+/*
+ * Function: reconnect
+ * Attempts to reestablish the connection to the memento site.
+ */
+var connectToMemento = function(timeout) {
+    /*
+     * Function: doConnect
+     * Attempts to open a tcp connection to the memento site. If it fails,
+     * connectToMemento will be called with a slightly longer timout.
+     */
+    var doConnect = function() {
+        console.log('found timeout: ' + timeout.toString());
+        mementoTCP = net.connection({port: MEMENTO_SITE_PORT, host: MEMENTO_SITE_HOST}, function() {
+            console.log('connected to the memento site.');
+            timeout = 50;
+        });
+
+        /*
+         * Event: mementoTCP 'data'
+         * When we recieve data from memento, broadcast it to all the tcpGuest
+         * connections.
+         */
+        mementoTCP.on('data', function(data) {
+            console.log('recieved data from memento site: ' + data);
+
+            for (g in tcpGuests) {
+                tcpGuests[g].write(data);
+            }
+        });
+
+        /*
+         * Event: mementoTCP 'end'
+         * If the connection is cut off, try to reestablish it.
+         */
+        mementoTCP.on('end', function() {
+            console.log('lost connection to memento site!');
+            connectToMemento(timeout*2);
+        });
+
+        /*
+         * Event: mementoTCP 'close'
+         * If the connection is cut off, try to reestablish it.
+         */
+        mementoTCP.on('close', function(hadError) {
+            console.log('Memento site connection closed.');
+            if (hadError) {
+                console.log('Memento site connection closed because of error: ');
+            }
+            connectToMemento(timeout*2);
+        });
+    };
+
+    console.log('attempting to connect to memento...');
+    setTimeout(doConnect, timeout);
 };
-
-server.listen(8080);
-
-var io = io.listen(server);
-io.on('connection', function(webSocket){
-
-  webGuests.push(webSocket);
-  webSocket.on('message', function(message){
-	console.log(message);
-
-    for (g in tcpGuests) {
-        tcpGuests[g].write(message);
-    }
-  });
-
-});
-
 
 var tcpServer = net.createServer(function (tcpSocket) {});
 
@@ -67,4 +95,8 @@ tcpServer.on('connection',function(tcpSocket){
         }
     })
 });
+
+// listen for the arduino connection
 tcpServer.listen(1337);
+// connect to the memento site
+connectToMemento();
