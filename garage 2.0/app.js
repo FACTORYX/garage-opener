9ec814deff76a340506d91ed8e206b1ea7c2993c
Marwan Hilmi <mhilmi@gmail.com>
2012-07-24 18:38:19 -0700

    Sockets + Socket

--- garage 2.0/app.js
+++ garage 2.0/app.js
@@ -1,6 +1,10 @@
-//this app.js will run on the 711 Scott network locally
-//it will connected to the arduino via a tcp socket
-//it will connect to the external world via a socket.io, port opened on network
+/*********************************************************\
+Project: m3m3n70.com 
+File: app.js
+Description: TCP Forwarder between Arduino and m3m3n70.com
+Authors: Turner Bohlen (turnerbohlen.com), Marwan Hilmi
+Copyright 2012 Scott St. Technology Company
+\*********************************************************/
 
 var http = require('http')
   , net = require('net')
@@ -8,24 +12,33 @@ var http = require('http')
   , fs = require('fs')
   , sys = require(process.binding('natives').util ? 'util' : 'sys')
   , server;
-
-var tcpGuest = null;
-var mementoTCP = null;
+  
+var deviceSockets = [];    //We can have multiple local devices sending data to this single server
+var mementoSocket = null;  //Only the one "homebase" server for now
 var MEMENTO_SITE_PORT;
 var MEMENTO_SITE_HOST;
 
 if (process.env.NODE_ENV === 'production') {
     MEMENTO_SITE_PORT = 4116;
-    MEMENTO_SITE_HOST = '';
+    MEMENTO_SITE_HOST = '50.97.248.62';
 }
 else if (process.env.NODE_ENV === 'development') {
     MEMENTO_SITE_PORT = 4116;
-    MEMENTO_SITE_HOST = '';
+    MEMENTO_SITE_HOST = '50.97.248.62';
+}
+else if (process.env.NODE_ENV === 'test'){
+    MEMENTO_SITE_PORT = 4116;
+    MEMENTO_SITE_HOST = '10.0.0.25';
 }
 else  {
     MEMENTO_SITE_PORT = 4116;
-    MEMENTO_SITE_HOST = '';
+    MEMENTO_SITE_HOST = '10.0.0.25';
 }
+
+
+////////////////////////////////////////////////////////////////////////////////
+//////////////////////// m3m3n70.com TCP SOCKET CONNECTION /////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
 /*
  * Function: reconnect
@@ -39,21 +52,23 @@ var connectToMemento = function(timeout) {
      */
     var doConnect = function() {
         console.log('found timeout: ' + timeout.toString());
-        mementoTCP = net.connection({port: MEMENTO_SITE_PORT, host: MEMENTO_SITE_HOST}, function() {
-            console.log('connected to the memento site.');
-            timeout = 50;
+	console.log('connecting to: ' + MEMENTO_SITE_HOST);
+        mementoSocket = net.connect(MEMENTO_SITE_PORT, MEMENTO_SITE_HOST, function() {
+            console.log('Connected to the m3m3n70!');
+	    timeout = 50;
         });
 
         /*
-         * Event: mementoTCP 'data'
+         * Event: mementoSocket'data'
          * When we recieve data from memento, broadcast it to all the tcpGuest
          * connections.
          */
-        mementoTCP.on('data', function(data) {
-            console.log('recieved data from memento site: ' + data);
+        mementoSocket.on('data', function(data) {
+            console.log('recieved data from m3m3n70: ' + data);
 
-            for (g in tcpGuests) {
-                tcpGuests[g].write(data);
+            for (g in deviceSockets) {
+		console.log('Sending data to device socket: ' + g);
+                deviceSockets[g].write(data);
             }
         });
 
@@ -61,8 +76,8 @@ var connectToMemento = function(timeout) {
          * Event: mementoTCP 'end'
          * If the connection is cut off, try to reestablish it.
          */
-        mementoTCP.on('end', function() {
-            console.log('lost connection to memento site!');
+        mementoSocket.on('end', function() {
+            console.log('lost connection to m3m3n70!');
             connectToMemento(timeout*2);
         });
 
@@ -70,40 +85,64 @@ var connectToMemento = function(timeout) {
          * Event: mementoTCP 'close'
          * If the connection is cut off, try to reestablish it.
          */
-        mementoTCP.on('close', function(hadError) {
-            console.log('Memento site connection closed.');
+        mementoSocket.on('close', function(hadError) {
+            console.log('m3m3n70 site connection closed!');
             if (hadError) {
-                console.log('Memento site connection closed because of error: ');
+                console.log('m3m3n70 site connection closed because of error: ');
             }
             connectToMemento(timeout*2);
         });
     };
 
-    console.log('attempting to connect to memento...');
+    console.log('Attempting to connect to m3m3n70...');
     setTimeout(doConnect, timeout);
 };
 
-//create tcp socket server on 1337
-var tcpServer = net.createServer(function (tcpSocket) {});
+////////////////////////////////////////////////////////////////////////////////
+//////////////////////// ARDUINO TCP SOCKET SERVER /////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+//TODO : Buffer data if m3m3n70 connection drops
 
-tcpServer.on('connection',function(tcpSocket){
-    console.log('num of connections on port 1337: ' + tcpServer.connections);
-    //also, not needed, we will only have tcp client
-	tcpGuests.push(tcpSocket);
-    
+// Sanitize Text for processing/parsing
+function sanitizeText(text) {
+	return text.toString().replace(/(\r\n|\n|\r)/gm,"");
+}
 
-    tcpSocket.on('data',function(data){
-        console.log('received on tcp socket:'+data);
-        for (g in webGuests) {
-			//write data directly to the web sockets
-            var webSocket = webGuests[g];
-            webSocket.send(data.toString('ascii',0,data.length));
-            
-        }
-    })
-});
+// Socket Data
+function receiveData(socket, data) {
+	console.log('Received data on device socket: '+data);
+	var cleanData = sanitizeText(data);
+	//Do something with cleanData if needed
+	if(mementoSocket) {
+		mementoSocket.write(data);
+	}
+}
+ 
+// Socket Closed
+function closeSocket(socket) {
+	//Check for socket and delete
+	var i = deviceSockets.indexOf(socket);
+	if (i != -1) {
+		sockets.splice(i, 1);
+	}
+}
+ 
+// Socket Opened
+function openSocket(socket) {
+	deviceSockets.push(socket);
+	socket.on('data', function(data) {
+		receiveData(socket, data);
+	})
+	socket.on('end', function() {
+		closeSocket(socket);
+	})
+}
 
-// listen for the arduino connection
-tcpServer.listen(1337);
-// connect to the memento site
-connectToMemento();
+
+
+//Create TCP Server for devices on 1337
+var deviceSocketServer = net.createServer(openSocket);
+deviceSocketServer.listen(1337);
+//Connect to Memento first ... ?
+connectToMemento(1000);
+
